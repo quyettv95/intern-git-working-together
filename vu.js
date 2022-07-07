761e09dd115d2b0b83a97a431ae47c2a7bebd218
Anh Vu <anhvuvqdh@gmail.com>
2022-07-07 09:22:56 +0700

    vu commit

---
+++ vu.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 5, 4];
+let flag = true;
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = i; j < arr.length; j++) {
+    if (arr[i] > arr[j]) {
+      flag = false;
+    }
+  }
+}
+console.log(flag);
