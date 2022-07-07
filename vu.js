1c725ede8069c8e83ca3cfe007474549b703a8f6
Anh Vu <anhvuvqdh@gmail.com>
2022-07-07 13:19:20 +0700

    sua file

--- vu.js
+++ vu.js
@@ -1,6 +1,6 @@
 const arr = [1, 2, 3, 4, 5];
 let flag = true;
-for (let i = 0; i < arr.length - 1; i++) {
+for (let i = 0; i < arr.length; i++) {
   if (arr[i] > arr[i + 1]) {
     flag = false;
   }
