8572cd3d69e3dbdc6cd187050bc1d601d8de733c
=Nguyen Manh Dung <dungli1221@gmail.com>
2022-08-02 09:26:10 +0700

    Dung-ailatrieuphu

---
+++ Dung ai la trieu phu js/main.js
@@ -0,0 +1,393 @@
+
+ 
+ const listquestions = [
+  {
+    title : "Câu 1: Mạnh vì…, bạo vì tiền ",
+    options : {
+      a: "A : Gạo",
+      b: "B : Bão",
+      c: "C : Mao",
+      d: "D : Sao",
+    },
+    answer: "A : Gạo",
+    score : 200000,
+  },
+  {
+    title : "Câu 2: Ngôi chùa được đúc hoàn toàn bằng đồng tại đất nước ta ?  ",
+    options : {
+      a: "A : Chùa Hương",
+      b: "B : Chùa Một Cột",
+      c: "C : Chùa Đồng",
+      d: "D : Chùa Đại Tuệ",
+    },
+    answer: "C : Chùa Đồng",
+    score : 400000,
+  },
+  {
+    title : "Câu 3: Người đẹp vì lụa, … tốt vì phân",
+    options : {
+      a: "A : Tiền",
+      b: "B : Áo",
+      c: "C : Quần",
+      d: "D : Lúa",
+    },
+    answer: "D : Lúa",
+    score : 600000,
+  },
+  {
+    title : "Câu 4: Người ta thường gọi đất nước nào là quốc gia mặt trời mọc ? ",
+    options : {
+      a: "A : Hàn Quốc",
+      b: "B : Nhật Bản",
+      c: "C : Việt Nam",
+      d: "D : Senegan",
+    },
+    answer:  "B : Nhật Bản",
+    score : 800000,
+  },
+  {
+    title : "Câu 5: Đâu là một loại hình chợ tạm tự phát thường xuất hiện trong các khu dân cư?",
+    options : {
+      a: "A : Ếch",
+      b: "B : Nhái",
+      c: "C : Thằn lằn",
+      d: "D : Cóc",
+    },
+    answer: "D : Cóc",
+    score : 1000000,
+  },
+  {
+    title : "Câu 6:Đâu là tên một bãi biển ở Quảng Bình?",
+    options : {
+      a: "A : Đá Lăn",
+      b: "B : Đá Chạy",
+      c: "C : Đá Nhảy",
+      d: "D : Đá Bò",
+    },
+    answer: "C : Đá Nhảy",
+    score : 1200000,
+  },
+  {
+    title : "Câu 7: Vợ của nghệ sĩ hài Xuân Bắc có tên là gì ",
+    options : {
+      a: "A : Cẩm chướng",
+      b: "B : Tuylip",
+      c: "C : Mào gà",
+      d: "D : Hồng Nhung",
+    },
+    answer: "D : Hồng Nhung",
+    score : 1400000,
+  },
+  {
+    title : "Câu 8: Chiến trường Đắk Tô - Tân Cảnh, nơi diễn ra chiến thắng vang đội năm 1972, nay thuộc địa bàn tỉnh nào ở Tây Nguyên? ",
+    options : {
+      a: "A :  Kon Tum",
+      b: "B : Gia Lai",
+      c: "C : Đắk Lắk",
+      d: "D : Đắk Nông",
+    },
+    answer: "A :  Kon Tum",
+    score : 1600000,
+  },
+  {
+    title : "Câu 9: Đâu là tên một loại bánh Huế?",
+    options : {
+      a: "A : Thích",
+      b: "B : Sướng",
+      c: "C : Khoái",
+      d: "D : Vui",
+    },
+    answer: "C : Khoái",
+    score : 1800000,
+  },
+  {
+    title : "Câu 10: Đâu không phải là một tác phẩm của họa sĩ Trần Văn Cẩn?",
+    options : {
+      a: "A : Đôi bạn",
+      b: "B : Mẹ",
+      c: "C : Em gái tôi",
+      d: "D : Em Thúy",
+    },
+    answer: "A : Đôi bạn",
+    score : 2000000,
+  },
+  {
+    title : "Câu 11: Nhà văn Kim Dung (Trung Quốc) nổi tiếng với thể loại văn học gì? ",
+    options : {
+      a: "A : Truyện trinh thám",
+      b: "B : Thơ lãng mạn",
+      c: "C : Sử thi",
+      d: "D : Tiểu thuyết kiếm hiệp",
+    },
+    answer: "D : Tiểu thuyết kiếm hiệp",
+    score : 2200000,
+  },
+  {
+    title : "Câu 12: Hoa hậu Hòa bình Quốc tế 2017 được tổ chức tại quốc gia nào? ",
+    options : {
+      a: "A :  Thái Lan",
+      b: "B : Việt Nam",
+      c: "C :  Campuchia",
+      d: "D : Lào",
+    },
+    answer: "B : Việt Nam",
+    score : 2400000,
+  },
+  {
+    title : "Câu 13:Trái Đất cách Mặt Trời khoảng bao nhiêu dặm? ",
+    options : {
+      a: "A :  9,3 triệu",
+      b: "B : 39 triệu",
+      c: "C :  193 triệu",
+      d: "D : 93 triệu",
+    },
+    answer: "D : 93 triệu",
+    score : 2600000,
+  },
+  {
+    title : "Câu 14: Các nhà thần kinh học tin rằng vỏ thùy giữa trán của não được kích hoạt khi bạn làm gì? ",
+    options : {
+      a: "A : Hiểu một chuyện cười.",
+      b: "B : Ghi nhớ một cái tên.",
+      c: "C : Bị hoảng loạn",
+      d: "D : Nghe nhạc.",
+    },
+    answer: "A : Hiểu một chuyện cười.",
+    score : 2800000,
+  },
+  {
+    title : "Câu 15: Tổng thống Mỹ nào từng xuất hiện trong loạt phim truyền hình Laugh-In?",
+    options : {
+      a: "A :  Lyndon Johnson",
+      b: "B : Sao",
+      c: "C : Richard Nixon",
+      d: "D : Jimmy Carter",
+    },
+    answer: "C : Richard Nixon",
+    score : 3000000,
+  },
+ ]; 
+ 
+ 
+ let currentQuestion =0;
+ let total =0;
+ //Time
+ let supportBtn = document.querySelector('.support-btn');
+let cntTime = 60;
+let timeElement = document.querySelector(".time");
+function timeLeft() {
+  if (cntTime > 0) {
+      timeElement.innerText = cntTime;
+      cntTime = cntTime - 1;
+  }
+  if (cntTime == 0) {
+    timeElement.innerText = 0;
+    setTimeout(function () {
+      alert(` Time out `);
+      document.querySelector(".exit").style.display="none";
+      document.querySelector(".start").style.display="block";
+      startBtn.style.display="block";
+    }, 1000);
+    currentQuestion = 0;
+    total =0;
+    cntTime = 60;
+  }
+}
+ 
+ // renderQuestion
+
+ 
+
+ function renderQuestion(currentQuestion){
+   questionBonusElement.innerHTML =  "Thưởng" + " : "  + listquestions[currentQuestion].score + " vnđ";
+   questionContentElement.innerHTML = listquestions[currentQuestion].title;
+   aAnswerElement.innerHTML = listquestions[currentQuestion].options.a;
+   bAnswerElement.innerHTML = listquestions[currentQuestion].options.b;
+   cAnswerElement.innerHTML = listquestions[currentQuestion].options.c;
+   dAnswerElement.innerHTML = listquestions[currentQuestion].options.d;
+  }
+  let renderTime;
+  // STart / Exit
+ const startBtn = document.querySelector('.start-btn');
+ const exitBtn=document.querySelector('.exit-btn');
+ 
+ function start(){
+  
+   document.querySelector(".exit").style.display="block";
+   document.querySelector(".start").style.display="none";
+  
+     total =0;
+     currentQuestion = 0;
+     renderTime = setInterval(timeLeft,1000);
+     document.querySelector(".total-money").innerText = "Tổng: " + total + " vnđ";
+     supportBtn.disabled = false;
+  supportBtn.style.cursor = "pointer";
+     renderQuestion(currentQuestion);
+  }
+    function exit(){
+      const isExit =confirm('Bạn thực sự muốn bỏ cuộc ?')
+      if(isExit==true){
+        document.querySelector(".exit").style.display="none";
+        document.querySelector(".start").style.display="block";
+        startBtn.style.display="block";
+        document.querySelector('.start-title').style.display="none";
+        currentQuestion =0; 
+        total =0;
+        cntTime =60;
+        timeElement.innerText = cntTime;
+        clearInterval(renderTime);
+      }
+    }
+    startBtn.addEventListener("click", function()
+    {
+      if(confirm('Bạn đã nắm rõ luật của chương trình và sẵn sàng chơi chưa ?'))
+      {
+      this.bgSound =new sound('bg.mp3');
+      this.bgSound.start();
+      startBtn.style.display="none";
+      document.querySelector('.start-title').style.display="block";
+      setTimeout(start , 30000);
+      cntTime = 60;
+      }
+    });
+    exitBtn.addEventListener("click", exit);
+  
+
+ //render question
+ let questionBonusElement = document.querySelector(".bonus");
+ let questionContentElement = document.querySelector(".question-text")
+  let aAnswerElement = document.querySelector("#a");
+  let bAnswerElement = document.querySelector("#b");
+  let cAnswerElement = document.querySelector("#c");
+  let dAnswerElement = document.querySelector("#d");
+let answerElements = document.querySelectorAll(".content-answer-item");
+
+  renderQuestion(currentQuestion);
+  
+  // check answer
+
+  for (let i = 0; i < answerElements.length; i++) {
+
+    answerElements[i].addEventListener("click", function () {
+      if(document.querySelectorAll('.hidden')){
+        document.querySelectorAll('.hidden').forEach(hidden => {
+          hidden.classList.remove('hidden');
+          })
+      }
+      if (currentQuestion == 14) {
+        if (answerElements[i].textContent == listquestions[14].answer) {
+          alert("Chúc mừng bạn đã vượt qua được bộ câu hỏi của chúng tôi - Bạn nhận được 24.000.000 vnđ");
+          this.bgSound= new sound('win.mp3');
+          this.bgSound.start();
+          document.querySelector(".heading").style.display="none";
+          document.querySelector(".content").style.display="none";
+
+          setTimeout(function () {
+            document.querySelector(".exit").style.display="none";
+            document.querySelector(".start").style.display="block";
+            startBtn.style.display="block";
+            document.querySelector('.start-title').style.display="none";
+            total = 0;
+            currentQuestion = 0;
+          }, 9000);
+
+
+        } else {
+          alert("Chúc mừng bạn đã nhận được : " + total + " vnđ");
+          total = 0;
+          currentQuestion = 0;
+     startBtn.style.display="block";
+     document.querySelector('.start-title').style.display="none";
+
+        }
+      } else {
+        if (answerElements[i].textContent == listquestions[currentQuestion].answer) {
+          this.bgSound = new sound('dung.mp3')
+          this.bgSound.start();
+          total += listquestions[currentQuestion].score;
+          currentQuestion += 1;
+          cntTime = 60;
+        } else {
+         
+          if (currentQuestion > 9) {
+            alert("Chúc mừng bạn đã nhận được :  " + total + " vnđ");
+            this.bgSound = new sound('c5.mp3');
+            this.bgSound.start();
+            document.querySelector(".heading").style.display="none";
+        document.querySelector(".content").style.display="none";
+        setTimeout(function() {
+          total = 0;
+          currentQuestion = 0;
+          document.querySelector(".exit").style.display="none";
+          document.querySelector(".start").style.display="block";
+   startBtn.style.display="block";
+   document.querySelector('.start-title').style.display="none";
+        },8000)
+          }
+        else if (currentQuestion > 4) {
+          alert("Chúc mừng bạn đã nhận được :  " + total + " vnđ");
+            this.bgSound = new sound('c5.mp3');
+            this.bgSound.start();
+            document.querySelector(".heading").style.display="none";
+        document.querySelector(".content").style.display="none";
+            setTimeout(function() {
+              total = 0;
+              currentQuestion = 0;
+              document.querySelector(".exit").style.display="none";
+              document.querySelector(".start").style.display="block";
+       startBtn.style.display="block";
+       document.querySelector('.start-title').style.display="none";
+            },8000)
+
+          } else {
+            alert("Rất tiếc khi phải nói lời tạm biệt với bạn tại đây");
+            this.bgSound = new sound('sai1.mp3')
+          this.bgSound.start();
+          document.querySelector(".heading").style.display="none";
+        document.querySelector(".content").style.display="none";
+          setTimeout(function() {
+
+            document.querySelector(".exit").style.display="none";
+            document.querySelector(".start").style.display="block";
+            startBtn.style.display="block";
+            document.querySelector('.start-title').style.display="none";
+            
+            currentQuestion = 0;
+            total = 0;
+          },7000);
+          }
+        } 
+      }
+  
+      document.querySelector(".total-money").innerText = "Tổng: " + total + " vnđ";
+      renderQuestion(currentQuestion);
+    });
+  }
+ 
+  
+ 
+
+  // 50 /50
+  let answerAll = document.querySelectorAll(".content-answer-item");
+
+supportBtn.addEventListener("click", function () {
+  let { a, b, c, d } = listquestions[currentQuestion].options;
+
+  let answers = [a, b, c, d];
+  let wrongAnswers = answers.filter(
+    (answer) => answer != listquestions[currentQuestion].answer
+  );
+  for (let i = 0; i < wrongAnswers.length - 1; i++) {
+    answerAll.forEach((answer) => {
+      if (wrongAnswers[i] == answer.textContent.trim()) {
+        answer.classList.add("hidden");
+      }
+    });
+  }
+  supportBtn.disabled = "true";
+  supportBtn.style.cursor = "not-allowed";
+});
+
+
+
+
